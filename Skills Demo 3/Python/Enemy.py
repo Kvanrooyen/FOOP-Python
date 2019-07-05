8103f8ddc337673227e505b7fdec942da447cc39
Keagan <governor.keagan@outlook.com>
2019-07-06 00:37:22 +0100

    Added Enemy class

--- Skills Demo 3/Python/Enemy.py
+++ Skills Demo 3/Python/Enemy.py
@@ -12,3 +12,33 @@ Must have an additional method:
 
 Must have only one constructor - default, no arguments
 """
+from Sprite import Sprite
+
+
+class Enemy(Sprite):
+
+    def __init__(self, teamName="Team name not set", playersKilled=0):
+        self.name = "Name not set"
+        self.xLocation = 0
+        self.yLocation = 0
+        self.isVisible = True
+        self.isAlive = True
+        self.teamName = teamName
+        self.playersKilled = playersKilled
+
+    def setTeamName(self, teamName):
+        self.teamName = teamName
+
+    def setPlayersKilled(self, playersKilled):
+        self.plaplayersKilled = playersKilled
+
+    def enemySpeak(self):
+        print("I am an enemy")
+
+    def getPlayerDetails(self):
+        return 'Team name: {0}\nPlayers killed: {1}'.format(self.teamName, self.playersKilled)
+
+    def attack(self):
+        self.xLocation += 10
+        print("I attacked, my location has been updated. \nX: {0} and Y: {1}".format(
+            self.xLocation, self.yLocation))
