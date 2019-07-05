0a216639ae55f8bb606b918a149bb0280a2f6d7f
Keagan <governor.keagan@outlook.com>
2019-07-06 00:02:47 +0100

    Added player class

--- Skills Demo 3/Python/Player.py
+++ Skills Demo 3/Python/Player.py
@@ -12,3 +12,33 @@ Must have an additional method:
 
 Must have one constructor - default, no arguments
 """
+from Sprite import Sprite
+
+
+class Player(Sprite):
+
+    def __init__(self, teamName="Team name not set", enemiesKilled=0):
+        self.name = "Name not set"
+        self.xLocation = 0
+        self.yLocation = 0
+        self.isVisible = True
+        self.isAlive = True
+        self.teamName = teamName
+        self.enemiesKilled = enemiesKilled
+
+    def setTeamName(self, teamName):
+        self.teamName = teamName
+
+    def setEnemiesKilled(self, enemiesKilled):
+        self.enemiesKilled = enemiesKilled
+
+    def playerSpeak(self):
+        print("I am a player")
+
+    def getPlayerDetails(self):
+        return 'Team name: {0}\nEnemies killed: {1}'.format(self.teamName, self.enemiesKilled)
+
+    def retreat(self):
+        self.xLocation -= 10
+        print("I retreated, my location has been updated. \nX: {0} and Y: {1}".format(
+            self.xLocation, self.yLocation))
