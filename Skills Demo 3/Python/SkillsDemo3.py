ecca1970278f74fcc2dcea4e122f45cc6ae505eb
Keagan <governor.keagan@outlook.com>
2019-07-06 00:44:01 +0100

    Added enemy test

--- Skills Demo 3/Python/SkillsDemo3.py
+++ Skills Demo 3/Python/SkillsDemo3.py
@@ -8,6 +8,7 @@ Get player to retreat and print out X and Y location
 """
 from Sprite import Sprite
 from Player import Player
+from Enemy import Enemy
 
 # Player Test
 player1 = Player()
@@ -18,3 +19,13 @@ player1.setTeamName("Red Dragons")
 print(player1.getPlayerDetails())
 print(player1.__str__())
 player1.retreat()
+
+# Enemy Test
+enemy1 = Enemy()
+enemy1.setName("Jo")
+enemy1.setLocation(65464, 45647)
+enemy1.setPlayersKilled(4)
+enemy1.setTeamName("Eagles")
+print(enemy1.getPlayerDetails())
+print(enemy1.__str__())
+enemy1.attack()
