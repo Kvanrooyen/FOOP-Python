d3048739249778220af9d61d7a7f92620aa85060
Keagan <governor.keagan@outlook.com>
2019-07-05 22:36:40 +0100

    Updated to use __str__ when returning values. Changed functions to be used to set v alues.

--- Skills Demo 3/Python/SkillsDemo3.py
+++ Skills Demo 3/Python/SkillsDemo3.py
@@ -7,11 +7,29 @@ Get enemy to attack and print out X and Y location
 Get player to retreat and print out X and Y location
 """
 from Sprite import Sprite
+from Player import Player
 
 
-# # Test Sprite
-# sprite1 = Sprite()
-# print(sprite1.currentLocation())
-# sprite1.jump()
-# print(sprite1.currentLocation())
-# sprite1.speak()
+# Test Sprite
+sprite1 = Sprite()
+sprite1.setName("Keagan")
+sprite1.setLocation(654, 52)
+sprite1.speak()
+print(sprite1.__str__())
+sprite1.jump()
+print('\n\n' + sprite1.__str__())
+
+
+# Player Test
+player1 = Player()
+# FIXME If no value is given program crashes. Should revert back to default value
+print(player1.playerName("Player One"))
+print(player1.playerLocation(25, 45))
+player1.playerJump()
+player1.retreat()
+# FIXME If no value is given program crashes. Should use
+print(player1.playerLocation())
+player1.speak()
+print(player1.playerTeam("Good Team"))
+player1.playerAlive(False)
+player1.playerVisible(True)
